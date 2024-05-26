1a481700d32773bf40b184c5ecf90072a12c6126
christianlulaj <lulajchristian@gmail.com>
2024-05-26 10:47:23 -0400

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
   const linkClass = ({ isActive }) =>
@@ -8,14 +7,12 @@ const Navbar = () => {
       : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
 
   return (
-    <nav className='bg-indigo-700 border-b border-indigo-500'>
-      <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+    <nav className='bg-slate-700 border-b border-indigo-500'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            <NavLink className='flex flex-shrink-0 items-center mr-4' to='/'>
-              <img className='h-10 w-auto' src={logo} alt='React Jobs' />
+            <NavLink className='flex flex-shrink-0 items-center mr-4 mx-4' to='/'>
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
-                React Jobs
+                Technical Solutions Inc.
               </span>
             </NavLink>
             <div className='md:ml-auto'>
@@ -23,17 +20,19 @@ const Navbar = () => {
                 <NavLink to='/' className={linkClass}>
                   Home
                 </NavLink>
-                <NavLink to='/jobs' className={linkClass}>
-                  Jobs
+                <NavLink to='/appointmnet' className={linkClass}>
+                   Appointment
                 </NavLink>
-                <NavLink to='/add-job' className={linkClass}>
-                  Add Job
+                <NavLink to='/checkout' className={linkClass}>
+                   Checkout
+                </NavLink>
+                <NavLink to='/login' className={linkClass}>
+                  Login
                 </NavLink>
               </div>
             </div>
           </div>
         </div>
-      </div>
     </nav>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,10 @@
-import Hero from '../components/Hero';
-import HomeCards from '../components/HomeCards';
-import JobListings from '../components/JobListings';
-import ViewAllJobs from '../components/ViewAllJobs';
+import CreateAccountPage from "./CreateAccountPage";
+
 
 const HomePage = () => {
   return (
     <>
-      <Hero />
-      <HomeCards />
-      <JobListings isHome={true} />
-      <ViewAllJobs />
+      <CreateAccountPage />
     </>
   );
 };
